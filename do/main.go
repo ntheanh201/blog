26b45122c4c78888e2f84d358f25052cc488aac5
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-10-03 17:05:12 -0700

    convert s/prevew.ps1 and s/preview-static.ps1 to 'do -prevew' and 'do -preview-static'

--- do/main.go
+++ do/main.go
@@ -2,22 +2,49 @@ package main
 
 import (
 	"flag"
+	"os"
 	"os/exec"
 )
 
 var (
-	flgBuild bool
-	flgWc    bool
+	flgBuild         bool
+	flgPreview       bool
+	flgPreviewStatic bool
+	flgWc            bool
 )
 
 func parseFlags() {
 	flag.BoolVar(&flgBuild, "build", false, "build the executable")
 	flag.BoolVar(&flgWc, "wc", false, "wc -l i.e. line count")
+	flag.BoolVar(&flgPreview, "preview", false, "preview on demand (rebuild html on access)")
+	flag.BoolVar(&flgPreviewStatic, "preview-static", false, "preview static (rebuild html first)")
 	flag.Parse()
 }
 
 func build() {
-	cmd := exec.Command("go", "build", ".")
+	cmd := exec.Command("go", "build", "-o", "blog_app.exe", ".")
+	runCmd(cmd)
+}
+
+func preview() {
+	exeName := "./blog_app.exe"
+	cmd := exec.Command("go", "build", "-o", exeName, ".")
+	runCmd(cmd)
+	cmd = exec.Command(exeName, "-preview-on-demand")
+	defer os.Remove(exeName)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	runCmd(cmd)
+}
+
+func previewStatic() {
+	exeName := "./blog_app.exe"
+	cmd := exec.Command("go", "build", "-o", exeName, ".")
+	runCmd(cmd)
+	cmd = exec.Command(exeName, "-preview")
+	defer os.Remove(exeName)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	runCmd(cmd)
 }
 
@@ -29,6 +56,16 @@ func main() {
 
 	if flgBuild {
 		build()
+		return
+	}
+
+	if flgPreview {
+		preview()
+		return
+	}
+
+	if flgPreviewStatic {
+		previewStatic()
 		return
 	}
 
