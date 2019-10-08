46076cc858d400c36611bfd62ba55ae29cbb9190
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-10-07 22:25:49 -0700

    update do util.go

--- do/util.go
+++ do/util.go
@@ -11,10 +11,11 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
+
+	"github.com/dustin/go-humanize"
 )
 
 var (
-	verbose bool
 	logFile io.Writer
 )
 
@@ -52,13 +53,13 @@ func logf(format string, args ...interface{}) {
 	if len(args) == 0 {
 		fmt.Print(format)
 		if logFile != nil {
-			fmt.Fprint(logFile, format)
+			_, _ = fmt.Fprint(logFile, format)
 		}
 		return
 	}
 	fmt.Printf(format, args...)
 	if logFile != nil {
-		fmt.Fprintf(logFile, format, args...)
+		_, _ = fmt.Fprintf(logFile, format, args...)
 	}
 }
 
@@ -79,20 +80,10 @@ func openBrowser(url string) {
 	must(err)
 }
 
-func readZipFile(path string) map[string][]byte {
-	r, err := zip.OpenReader(path)
+func getHomeDir() string {
+	s, err := os.UserHomeDir()
 	must(err)
-	defer r.Close()
-	res := map[string][]byte{}
-	for _, f := range r.File {
-		rc, err := f.Open()
-		must(err)
-		d, err := ioutil.ReadAll(rc)
-		must(err)
-		rc.Close()
-		res[f.Name] = d
-	}
-	return res
+	return s
 }
 
 // absolute path of the current directory
@@ -136,7 +127,7 @@ func dirExists(path string) bool {
 }
 
 func recreateDir(dir string) {
-	os.RemoveAll(dir)
+	_ = os.RemoveAll(dir)
 	err := os.MkdirAll(dir, 0755)
 	must(err)
 }
@@ -163,21 +154,41 @@ func writeFile(path string, data []byte) {
 	must(err)
 }
 
-func openCodeDiff(path1, path2 string) {
-	if runtime.GOOS == "darwin" {
-		path1 = strings.Replace(path1, ".\\", "./", -1)
-		path2 = strings.Replace(path2, ".\\", "./", -1)
-	}
-	cmd := exec.Command("code", "--new-window", "--diff", path1, path2)
-	logf("running: %s\n", strings.Join(cmd.Args, " "))
-	err := cmd.Start()
+func ls(path string) {
+	fi, err := os.Stat(path)
 	must(err)
+	sizeStr := humanize.Bytes(uint64(fi.Size()))
+	fmt.Printf("%s %s\n", path, sizeStr)
+}
+
+func cpFile(dstPath, srcPath string) {
+	d, err := ioutil.ReadFile(srcPath)
+	must(err)
+	err = ioutil.WriteFile(dstPath, d, 0666)
+	must(err)
+}
+
+func removeFile(path string) {
+	err := os.Remove(path)
+	if err == nil {
+		logf("removeFile('%s')", path)
+		return
+	}
+	if os.IsNotExist(err) {
+		// TODO: maybe should print note
+		return
+	}
+	fmt.Printf("os.Remove('%s') failed with '%s'\n", path, err)
 }
 
 func readFile(path string) []byte {
 	d, err := ioutil.ReadFile(path)
 	must(err)
 	return d
+}
+
+func fileClose(f io.Closer) {
+	_ = f.Close()
 }
 
 func areFilesEuqal(path1, path2 string) bool {
@@ -192,7 +203,18 @@ func openNotepadWithFile(path string) {
 	must(err)
 }
 
-func runCmd(cmd *exec.Cmd) {
+func openCodeDiff(path1, path2 string) {
+	if runtime.GOOS == "darwin" {
+		path1 = strings.Replace(path1, ".\\", "./", -1)
+		path2 = strings.Replace(path2, ".\\", "./", -1)
+	}
+	cmd := exec.Command("code", "--new-window", "--diff", path1, path2)
+	logf("running: %s\n", strings.Join(cmd.Args, " "))
+	err := cmd.Start()
+	must(err)
+}
+
+func runCmd(cmd *exec.Cmd) string {
 	fmt.Printf("> %s\n", cmd)
 	canCapture := (cmd.Stdout == nil) && (cmd.Stderr == nil)
 	if canCapture {
@@ -201,16 +223,121 @@ func runCmd(cmd *exec.Cmd) {
 			if len(out) > 0 {
 				fmt.Printf("Output:\n%s\n", string(out))
 			}
-			return
+			return string(out)
 		}
 		fmt.Printf("cmd '%s' failed with '%s'. Output:\n%s\n", cmd, err, string(out))
 		must(err)
+		return string(out)
 	}
 	err := cmd.Run()
 	if err == nil {
-		return
+		return ""
 	}
 	fmt.Printf("cmd '%s' failed with '%s'\n", cmd, err)
 	must(err)
+	return ""
+}
 
+func gitPull(dir string) {
+	cmd := exec.Command("git", "pull")
+	if dir != "" {
+		cmd.Dir = dir
+	}
+	runCmd(cmd)
+}
+
+func gitStatus(dir string) string {
+	cmd := exec.Command("git", "status")
+	if dir != "" {
+		cmd.Dir = dir
+	}
+	return runCmd(cmd)
+}
+
+func checkGitClean(dir string) {
+	s := gitStatus(dir)
+	expected := []string{
+		"On branch master",
+		"Your branch is up to date with 'origin/master'.",
+		"nothing to commit, working tree clean",
+	}
+	for _, exp := range expected {
+		if !strings.Contains(s, exp) {
+			fmt.Printf("Git repo in '%s' not clean.\nDidn't find '%s' in output of git status:\n%s\n", dir, exp, s)
+			os.Exit(1)
+		}
+	}
+}
+
+func readZipFile(path string) map[string][]byte {
+	r, err := zip.OpenReader(path)
+	must(err)
+	defer fileClose(r)
+	res := map[string][]byte{}
+	for _, f := range r.File {
+		rc, err := f.Open()
+		must(err)
+		d, err := ioutil.ReadAll(rc)
+		must(err)
+		_ = rc.Close()
+		res[f.Name] = d
+	}
+	return res
+}
+
+func zipAddFile(zw *zip.Writer, zipName string, path string) {
+	zipName = filepath.ToSlash(zipName)
+	d, err := ioutil.ReadFile(path)
+	must(err)
+	w, err := zw.Create(zipName)
+	_, err = w.Write(d)
+	must(err)
+	fmt.Printf("  added %s from %s\n", zipName, path)
+}
+
+func zipDirRecur(zw *zip.Writer, baseDir string, dirToZip string) {
+	dir := filepath.Join(baseDir, dirToZip)
+	files, err := ioutil.ReadDir(dir)
+	must(err)
+	for _, fi := range files {
+		if fi.IsDir() {
+			zipDirRecur(zw, baseDir, filepath.Join(dirToZip, fi.Name()))
+		} else if fi.Mode().IsRegular() {
+			zipName := filepath.Join(dirToZip, fi.Name())
+			path := filepath.Join(baseDir, zipName)
+			zipAddFile(zw, zipName, path)
+		} else {
+			path := filepath.Join(baseDir, fi.Name())
+			s := fmt.Sprintf("%s is not a dir or regular file", path)
+			panic(s)
+		}
+	}
+}
+
+func createZipFile(dst string, baseDir string, toZip ...string) {
+	removeFile(dst)
+	if len(toZip) == 0 {
+		panic("must provide toZip args")
+	}
+	fmt.Printf("Creating zip file %s\n", dst)
+	w, err := os.Create(dst)
+	must(err)
+	defer fileClose(w)
+	zw := zip.NewWriter(w)
+	must(err)
+	for _, name := range toZip {
+		path := filepath.Join(baseDir, name)
+		fi, err := os.Stat(path)
+		must(err)
+		if fi.IsDir() {
+			zipDirRecur(zw, baseDir, name)
+		} else if fi.Mode().IsRegular() {
+			zipAddFile(zw, name, path)
+		} else {
+			s := fmt.Sprintf("%s is not a dir or regular file", path)
+			panic(s)
+		}
+	}
+	err = zw.Close()
+	must(err)
 }
