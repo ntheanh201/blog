28ed744332fbb299f7d0f8c074c8d160a18721a1
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2021-09-14 16:55:06 -0700

    go snippets cheatsheet update

--- util.go
+++ util.go
@@ -6,13 +6,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net"
-	"net/http"
 	"os/exec"
 	"path/filepath"
 	"runtime"
 	"strings"
-	"time"
 )
 
 func panicIf(cond bool, args ...interface{}) {
@@ -229,42 +226,6 @@ func readFileMust(path string) []byte {
 	d, err := ioutil.ReadFile(path)
 	must(err)
 	return d
-}
-
-// can be used for http.Get() requests with better timeouts. New one must be created
-// for each Get() request
-func newTimeoutClient(connectTimeout time.Duration, readWriteTimeout time.Duration) *http.Client {
-	timeoutDialer := func(cTimeout time.Duration, rwTimeout time.Duration) func(net, addr string) (c net.Conn, err error) {
-		return func(netw, addr string) (net.Conn, error) {
-			conn, err := net.DialTimeout(netw, addr, cTimeout)
-			if err != nil {
-				return nil, err
-			}
-			conn.SetDeadline(time.Now().Add(rwTimeout))
-			return conn, nil
-		}
-	}
-
-	return &http.Client{
-		Transport: &http.Transport{
-			Dial:  timeoutDialer(connectTimeout, readWriteTimeout),
-			Proxy: http.ProxyFromEnvironment,
-		},
-	}
-}
-func httpPost(uri string, body []byte) ([]byte, error) {
-	// default timeout for http.Get() is really long, so dial it down
-	// for both connection and read/write timeouts
-	timeoutClient := newTimeoutClient(time.Second*120, time.Second*120)
-	resp, err := timeoutClient.Post(uri, "", bytes.NewBuffer(body))
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("'%s': status code not 200 (%d)", uri, resp.StatusCode)
-	}
-	return ioutil.ReadAll(resp.Body)
 }
 
 // from https://gist.github.com/hyg/9c4afcd91fe24316cbf0
---
+++ util_http.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+// can be used for http.Get() requests with better timeouts. New one must be created
+// for each Get() request
+func newTimeoutClient(connectTimeout time.Duration, readWriteTimeout time.Duration) *http.Client {
+	timeoutDialer := func(cTimeout time.Duration, rwTimeout time.Duration) func(net, addr string) (c net.Conn, err error) {
+		return func(netw, addr string) (net.Conn, error) {
+			conn, err := net.DialTimeout(netw, addr, cTimeout)
+			if err != nil {
+				return nil, err
+			}
+			conn.SetDeadline(time.Now().Add(rwTimeout))
+			return conn, nil
+		}
+	}
+
+	return &http.Client{
+		Transport: &http.Transport{
+			Dial:  timeoutDialer(connectTimeout, readWriteTimeout),
+			Proxy: http.ProxyFromEnvironment,
+		},
+	}
+}
+
+func httpPost(uri string, body []byte) ([]byte, error) {
+	// default timeout for http.Get() is really long, so dial it down
+	// for both connection and read/write timeouts
+	timeoutClient := newTimeoutClient(time.Second*120, time.Second*120)
+	resp, err := timeoutClient.Post(uri, "", bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("'%s': status code not 200 (%d)", uri, resp.StatusCode)
+	}
+	return ioutil.ReadAll(resp.Body)
+}
+
+func createMultiPartForm(form map[string]string) (string, io.Reader, error) {
+	body := new(bytes.Buffer)
+	mp := multipart.NewWriter(body)
+	defer mp.Close()
+	for key, val := range form {
+		if strings.HasPrefix(val, "@") {
+			val = val[1:]
+			file, err := os.Open(val)
+			if err != nil {
+				return "", nil, err
+			}
+			defer file.Close()
+			part, err := mp.CreateFormFile(key, val)
+			if err != nil {
+				return "", nil, err
+			}
+			io.Copy(part, file)
+		} else {
+			mp.WriteField(key, val)
+		}
+	}
+	return mp.FormDataContentType(), body, nil
+}
+
+func httpPostMultiPart(uri string, files map[string]string) ([]byte, error) {
+	contentType, body, err := createMultiPartForm(files)
+	if err != nil {
+		return nil, err
+	}
+	// default timeout for http.Get() is really long, so dial it down
+	// for both connection and read/write timeouts
+	timeoutClient := newTimeoutClient(time.Second*120, time.Second*120)
+	resp, err := timeoutClient.Post(uri, contentType, body)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("'%s': status code not 200 (%d)", uri, resp.StatusCode)
+	}
+	return ioutil.ReadAll(resp.Body)
+}
