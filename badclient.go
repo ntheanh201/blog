3fad2333b745a610faaba5373e4ab694c9335903
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2021-10-07 22:26:11 -0700

    update badclient.go

--- badclient.go
+++ badclient.go
@@ -22,6 +22,7 @@ var (
 		"/admin/controller/extension/extension/": true,
 		"/sites/default/files/":                  true,
 		"/.well-known/":                          true,
+		"/.env":                                  true,
 	}
 	badClientsContains = []string{
 		"/wp-login.php",
